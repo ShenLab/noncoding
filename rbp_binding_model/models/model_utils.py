c3cb282ffeb2a5ff423e10bd6dce4d794950a340
Emily Jin <ej2332@columbia.edu>
2020-06-20 22:19:29 -0400

    add code for models and organize directory

---
+++ rbp_binding_model/models/model_utils.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+#encode sequence into tensor
+base_dict = {
+    'A': np.array([1,0,0,0]),
+    'C': np.array([0,1,0,0]),
+    'G': np.array([0,0,1,0]),
+    'T': np.array([0,0,0,1]),
+    'N': np.array([0,0,0,0])
+}
+
+def encode_seq(sequences):
+  n = len(sequences)
+  a = np.empty((n,1000,4))
+  for i in range(n):
+    seq = sequences.iloc[i]
+    seq_array = list(seq)
+    for j in range(1000):
+      base = seq_array[j]
+      a[i][j] = base_dict[base]
+  return a
+
+#one hot encode annotation types
+annot_dict = {
+    'hg19_genes_introns': np.array([1,0,0,0,0]),
+    'hg19_genes_exons': np.array([0,1,0,0,0]),
+    'hg19_genes_1to5kb': np.array([0,0,1,0,0]),
+    'hg19_genes_promoters': np.array([0,0,0,1,0]),
+    'hg19_genes_5UTRs': np.array([0,0,0,0,1]),
+    'no_annot': np.array([0,0,0,0,0])
+}
+
+def encode_annot(annotations):
+  n = len(annotations)
+  a = np.empty((n,5))
+  for i in range(n):
+    a[i] = annot_dict[annotations.iloc[i]]
+  return a
+
+#format data correctly to input into model
+def get_formatted_data(X_df, Y_df):
+  seq_array = encode_seq(X_df['sequence'])
+  gene_array = X_df['lnTPM'].to_numpy()
+  annot_array = encode_annot(X_df['annot'])
+  label_array = Y_df['label'].to_numpy()
+  return seq_array, gene_array, annot_array, label_array
---
+++ rbp_binding_model/models/rnn_models.py
@@ -0,0 +1,108 @@
+import keras
+from keras.models import Sequential, Model
+from keras.layers import Dense, Flatten, MaxPooling1D, Conv1D, Dropout, LSTM, Input, Concatenate, Layer
+from keras.regularizers import l2
+from keras import backend as tf
+
+# CNN-LSTM combined model
+def get_cnn_rnn_model(max_len):
+  model_in = Input(shape=(max_len,4))
+  rank_in = Input(shape=(1,))
+  annot_in = Input(shape=(6,))
+
+  conv = Conv1D(100, kernel_size=8, activation='relu', input_shape=(max_len,4), 
+                    kernel_regularizer=l2(.01))(model_in)
+  pooling = MaxPooling1D(pool_size=4, strides=None, padding='valid')(conv)
+  drop1 = Dropout(.2)(pooling)
+
+  lstm = LSTM(50, return_sequences=True, activation='relu',
+                    kernel_regularizer=l2(.01))(drop1)
+  pooling2 = MaxPooling1D(pool_size=4, strides=None, padding='valid')(lstm)
+  drop2 = Dropout(.2)(pooling2)
+
+  flat = Flatten()(drop2)
+
+  dense1 = Dense(300, activation='relu')(flat)
+  dense2 = Dense(64, activation='relu')(dense1)
+
+  dense_rank = Dense(1,)(rank_in)
+  dense_annot = Dense(6,)(annot_in)
+
+  mult_layer_gene_annot = Concatenate()([dense2, dense_rank, dense_annot])
+  model_gene_annot_out = Dense(1, activation="sigmoid")(mult_layer_gene_annot)
+  model_gene_annot = Model(inputs=[model_in, rank_in, annot_in], outputs=model_gene_annot_out)
+
+  return model_gene_annot
+
+
+# Simple RNN model
+def get_rnn_model(max_len):
+  model_in = Input(shape=(max_len,4))
+  rank_in = Input(shape=(1,))
+  annot_in = Input(shape=(6,))
+
+  lstm = LSTM(50, return_sequences=True, activation='relu', input_shape=(max_len,4), kernel_regularizer=l2(.01))(model_in)
+  drop2 = Dropout(.5)(lstm)
+
+  flat = Flatten()(drop2)
+
+  dense1 = Dense(300, activation='relu')(flat)
+  dense2 = Dense(64, activation='relu')(dense1)
+
+  dense_rank = Dense(1,)(rank_in)
+  dense_annot = Dense(6,)(annot_in)
+
+  mult_layer_gene_annot = Concatenate()([dense2, dense_rank, dense_annot])
+  model_gene_annot_out = Dense(1, activation="sigmoid")(mult_layer_gene_annot)
+  model_gene_annot = Model(inputs=[model_in, rank_in, annot_in], outputs=model_gene_annot_out)
+
+  return model_gene_annot
+
+
+class attention(Layer):
+    def __init__(self,**kwargs):
+        super(attention,self).__init__(**kwargs)
+
+    def build(self,input_shape):
+        self.W=self.add_weight(name="att_weight",shape=(input_shape[-1],1),initializer="normal")
+        self.b=self.add_weight(name="att_bias",shape=(input_shape[1],1),initializer="zeros")        
+        super(attention, self).build(input_shape)
+
+    def call(self,x):
+        et=tf.squeeze(tf.tanh(tf.dot(x,self.W)+self.b),axis=-1)
+        at=tf.softmax(et)
+        at=tf.expand_dims(at,axis=-1)
+        output=x*at
+        return tf.sum(output,axis=1)
+
+    def compute_output_shape(self,input_shape):
+        return (input_shape[0],input_shape[-1])
+
+    def get_config(self):
+        return super(attention,self).get_config()
+
+
+def get_rnn_attention_model(max_len):
+  model_in = Input(shape=(max_len,4))
+  rank_in = Input(shape=(1,))
+  annot_in = Input(shape=(6,))
+
+  conv = Conv1D(100, kernel_size=8, activation='relu', input_shape=(max_len,4), kernel_regularizer=l2(.01))(model_in)
+  pooling = MaxPooling1D(pool_size=4, strides=None, padding='valid')(conv)
+  drop1 = Dropout(.2)(pooling)
+
+  att_in = LSTM(50, return_sequences=True, activation='relu', kernel_regularizer=l2(.01))(drop1)
+  att_out = attention()(att_in)
+  drop2 = Dropout(.2)(att_out)
+
+  dense1 = Dense(300, activation='relu')(drop2)
+  dense2 = Dense(64, activation='relu')(dense1)
+
+  dense_rank = Dense(1,)(rank_in)
+  dense_annot = Dense(6,)(annot_in)
+
+  mult_layer_gene_annot = Concatenate()([dense2, dense_rank, dense_annot])
+  model_gene_annot_out = Dense(1, activation="sigmoid")(mult_layer_gene_annot)
+  model_gene_annot = Model(inputs=[model_in, rank_in, annot_in], outputs=model_gene_annot_out)
+
+  return model_gene_annot
